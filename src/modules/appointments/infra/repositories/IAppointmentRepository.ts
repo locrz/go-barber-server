c87d7a380a3c4fc8bf6e4aa7a843b533c4795afb
Locrz <locrz@MBP-de-Danilo-Pela.local>
2020-10-13 08:26:30 -0300

    Refactory appointment repository

--- src/modules/appointments/infra/repositories/IAppointmentRepository.ts
+++ src/modules/appointments/infra/repositories/IAppointmentRepository.ts
@@ -1,5 +1,7 @@
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
 
 export default interface IAppointmentRepository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,14 +1,27 @@
 import Appointment from "../entities/Appointment";
-import { EntityRepository, Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import IAppointmentRepository from "../../repositories/IAppointmentRepository";
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 
-@EntityRepository(Appointment)
-class AppointmentsRepository
-  extends Repository<Appointment>
-  implements IAppointmentRepository {
+class AppointmentsRepository implements IAppointmentRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const appointmentOnSameDate = await this.findOne({ where: date });
+    const appointmentOnSameDate = await this.ormRepository.findOne({
+      where: date,
+    });
     return appointmentOnSameDate;
+  }
+
+  public async create({ provider_id, date }: ICreateAppointmentDTO) {
+    const appointment = this.ormRepository.create({ provider_id, date });
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
