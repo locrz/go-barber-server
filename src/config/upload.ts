9636e2b23d72c6aa4eed3e601f2d9bd4614767ab
Locrz <locrz@MBP-de-Danilo-Pela.local>
2020-11-10 07:33:12 -0300

    create s3 provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,22 +1,39 @@
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
 import path from "path";
 import crypto from "crypto";
 
 const tmpDir = path.resolve(__dirname, "..", "..", "tmp");
 
+interface IStorageConfig {
+  driver: "s3" | "disk";
+
+  tmpDirectory: string;
+  uploadDirectory: string;
+
+  multer: { storage: StorageEngine };
+
+  config: { disk: {}; aws: { bucket: string } };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpDirectory: tmpDir,
   uploadDirectory: path.resolve(tmpDir, "uploads"),
 
-  storage: multer.diskStorage({
-    destination: tmpDir,
-    filename(request, file, callback) {
-      const originalNameWithoutSpaces = file.originalname.replace(/\s/g, "");
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpDir,
+      filename(request, file, callback) {
+        const originalNameWithoutSpaces = file.originalname.replace(/\s/g, "");
 
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}-${originalNameWithoutSpaces}`;
+        const fileHash = crypto.randomBytes(10).toString("hex");
+        const fileName = `${fileHash}-${originalNameWithoutSpaces}`;
 
-      return callback(null, fileName);
-    },
-  }),
-};
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: { disk: {}, aws: { bucket: "app-gobarber09" } },
+} as IStorageConfig;
--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,8 @@ import {
 } from "typeorm";
 import { Exclude, Expose } from "class-transformer";
 
+import uploadConfig from "@config/upload";
+
 @Entity("users")
 class User {
   @PrimaryGeneratedColumn("uuid")
@@ -31,9 +33,20 @@ class User {
   @UpdateDateColumn()
   updated_at: Date;
 
-  @Expose({name: 'avatar_url'})
+  @Expose({ name: "avatar_url" })
   getAvatarUrl() {
-    return this.avatar ? `${process.env.API_URL}/files/${this.avatar}` : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case "disk":
+        return `${process.env.API_URL}/files/${this.avatar}`;
+      case "s3":
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
